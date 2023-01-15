037a6a5026803ca123bf2969bc4494243754798b
Davi Patricio <davipatricio2006@gmail.com>
2023-01-15 21:01:35 +0000

    feat: add `astro-compress`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-    
+  integrations: [compress()]
 });
